ee08f5f4a52ca045404e0516d90b9d88f56599a9
unknown <hpeyal7@gmail.com>
2024-02-15 16:53:14 +0600

    complite game

--- alphabet-clash-pro-resources/script.js
+++ alphabet-clash-pro-resources/script.js
@@ -6,10 +6,40 @@
 //     playgroundSection.classList.remove('hidden')
 // }
 
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+   if(playerPressed === expectedAlphabet){
+    // start a new round:
+    // update the score.
+   const currentScore = getTextElementValueById('current-score')
+    const newScore = currentScore + 1;
+    setTextElementValueById('current-score', newScore)
+    removeBackgroundColorById(expectedAlphabet)
+    continueGame();
+
+   }
+   else{ 
+    const currentLife = getTextElementValueById('current-life')
+    const newLife = currentLife - 1;
+    setTextElementValueById('current-life', newLife)
+    if(newLife === 0){
+        gameOver();
+    }
+   }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
 
-    const currentAlphabetElement = document.getElementById('current-alphabte');
+    const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
 
     setBackgroundColorById(alphabet)
@@ -18,5 +48,19 @@ function continueGame(){
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    hideElementById('final-score');
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('total-score', lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet)
 }
--- alphabet-clash-pro-resources/utility.js
+++ alphabet-clash-pro-resources/utility.js
@@ -10,16 +10,38 @@ function showElementById(elementId){
 
 function getARandomAlphabet(){
     const alphabetString = 'asdfghjklpoiuytrewqzxcvbnm';
-    const aplhabets = alphabetString.split('');
+    const alphabets = alphabetString.split('');
     
     const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
-    const aplhabet =aplhabets[index];
-    return aplhabet;
+    const alphabet =alphabets[index];
+    return alphabet;
 }
 
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400')
     document.getElementById('current-alphabte').toogle
+}
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const currentValueText = element.innerText;
+    const value = parseInt(currentValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
